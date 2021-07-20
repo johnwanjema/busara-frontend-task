5327bca9c9e404aaf764730a33d9d6826779bb3c
John Wanjema <jonwanjema@gmail.com>
2021-07-20 11:57:21 +0300

    update footer description

--- components/footer.vue
+++ components/footer.vue
@@ -2,10 +2,7 @@
     <footer id="page-footer" class="opacity-0" style="opacity: 1;">
         <div class="content py-20 font-size-sm clearfix">
             <div class="float-right">
-                Crafted with <i class="fa fa-heart text-pulse"></i> by <a class="font-w600" href="https://1.envato.market/ydb" target="_blank">pixelcave</a>
-            </div>
-            <div class="float-left">
-                <a class="font-w600" href="https://1.envato.market/95j" target="_blank">Codebase 3.3</a> © <span class="js-year-copy js-year-copy-enabled">2021</span>
+                <a class="font-w600" href="https://busaracenter.org" target="_blank">Busara</a> © <span class="js-year-copy js-year-copy-enabled">2021</span>
             </div>
         </div>
     </footer>
