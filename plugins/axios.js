fa5a1a503927168b1d59fe87035a6324a525de40
John Wanjema <jonwanjema@gmail.com>
2021-07-21 16:56:05 +0300

    redirect to login if user is unauthenticated

--- plugins/axios.js
+++ plugins/axios.js
@@ -2,7 +2,7 @@ export default function ({ $axios, redirect }) {
     $axios.onError(error => {
       const code = parseInt(error.response && error.response.status)
       if (code === 401) {
-        console.log('unauthernticated')
+        redirect('/')
       }
     })
   }
