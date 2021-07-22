f9fb2de814e69c06d345ab60d897cd387e7d1ca3
John Wanjema <jonwanjema@gmail.com>
2021-07-22 07:08:34 +0300

    comment profile link

--- components/header.vue
+++ components/header.vue
@@ -21,9 +21,9 @@
                                                             </button>
                     <div class="dropdown-menu dropdown-menu-right min-width-200" aria-labelledby="page-header-user-dropdown">
                         <h5 class="h6 text-center py-10 mb-5 border-b text-uppercase">User</h5>
-                        <nuxt-link class="dropdown-item" to="/profile">
+                        <!-- <nuxt-link class="dropdown-item" to="/profile">
                             <i class="si si-user mr-5"></i> Profile
-                        </nuxt-link>
+                        </nuxt-link> -->
     
     
                         <div class="dropdown-divider"></div>
